3c9e43e2439dab24493653d17973e96feb2d930e
Jonathan Ackerman <jonathan.ackerman@digipen.edu>
2017-01-12 08:08:46 -0800

    update gitignore and added project file and square.png
    gitignore now ignores all the bs from unity,
    added .unity file,
    added square.png

---
+++ Project1_Prototype/Assets/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    string up_key = "w";
+    string left_key = "a";
+    string down_key = "s";
+    string right_key = "d";
+    int moveSpeed = 10;
+    Vector3 moveDir = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		if (Input.GetKey(up_key))
+        {
+            moveDir += new Vector3(0, moveSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(left_key))
+        {
+            moveDir += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(down_key))
+        {
+            moveDir += new Vector3(0, -moveSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(right_key))
+        {
+            moveDir += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        moveDir.Normalize();
+        GetComponent<Transform>().position = moveDir;
+	}
+}
