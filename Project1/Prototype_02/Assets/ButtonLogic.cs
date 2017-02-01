764c29cc723a17e427bc82668100d89cee924522
JonathanAckerman <jonathan.ackerman@digipen.edu>
2017-01-31 16:15:22 -0800

    game clock and main menu

---
+++ Project1/Prototype_02/Assets/ButtonLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonLogic : MonoBehaviour {
+
+	public void OnPlay()
+    {
+        SceneManager.LoadScene("DefaultScene");
+    }
+    public void OnHowTo()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+    public void OnBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Project1/Prototype_02/Assets/GameClock.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameClock : MonoBehaviour {
+    float GameTimer;
+    public float GameDuration;
+    public Text clockText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameTimer = GameDuration;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (GameTimer > 0.0f)
+        {
+            // Subtract DT
+            GameTimer -= Time.deltaTime;
+            // Ceil the value
+            int gameTime = (int)Mathf.Ceil(GameTimer);
+            int[] clockTime = SecondsToClockTime(gameTime);
+            // update the clock
+            clockText.text = clockTime[0].ToString() + ":" + (clockTime[1] >= 10 ? clockTime[1].ToString() : "0" + clockTime[1].ToString());
+        }
+        else
+        {
+            //Game End
+        }
+	}
+
+    int[] SecondsToClockTime(int seconds)
+    {
+        int[] time = new int[2];
+        time[0] = (int)Mathf.Floor(seconds / 60);
+        time[1] = seconds % 60;
+        return time;
+    }
+}
